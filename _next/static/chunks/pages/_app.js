6bf202e7e27093a548b9c571748f5ea79713007e
ivrylobs <mt.f.rk.shit@gmail.com>
2021-01-10 00:29:59 +0700

    Update repo

--- _next/static/chunks/pages/_app.js
+++ _next/static/chunks/pages/_app.js
@@ -10428,7 +10428,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_2__);
 
 
-var _jsxFileName = "E:\\Development\\Webapp\\ivrylobs.github.io\\pages\\_app.js";
+var _jsxFileName = "E:\\Development\\Webapp\\ivrylobs-home\\pages\\_app.js";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
@@ -10608,7 +10608,7 @@ module.exports = content.locals || {};
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=private-next-pages%2F_app.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=private-next-pages%2F_app.js!./");
-module.exports = __webpack_require__(/*! E:\Development\Webapp\ivrylobs.github.io\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! E:\Development\Webapp\ivrylobs-home\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ })
