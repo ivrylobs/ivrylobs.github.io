c4c4cd666509419346c2de6c1ca49b39cd13461f
Isaac Kun <mt.f.rk.shit@gmail.com>
2021-07-20 01:25:13 +0700

    Create API fetching test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <Tutorial/>
+  <b-container fluid="md">
+    <h1 v-if="posts === null">API endpoint is not available</h1>
+    <h1 v-else>Blog</h1>
+  </b-container>
 </template>
 
 <script>
-export default {}
+import axios from '@nuxtjs/axios'
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async fetch({ $axios }) {
+    try {
+      this.posts = await $axios.$get('http://localhost:3001/api/me')
+      console.log(this.posts)
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status)
+      } else {
+        console.log('API endpoint is not available')
+        this.posts = null
+      }
+    }
+  },
+}
 </script>
